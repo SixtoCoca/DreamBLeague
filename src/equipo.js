8e85bc54c45eae21e6e1fad2fe65cd9f296609b3
Sixto <sixtococacruz@gmail.com>
2020-10-07 13:01:29 +0200

    Modificado la clase

--- src/equipo.js
+++ src/equipo.js
@@ -1,12 +1,12 @@
 class Equipo{
-    constructor(){
-        var posicion;
-        var victorias;
-        var derrotas;
-        var puntos;
-        var asistencias;
-        var rebotesofensivos;
-        var rebotesdefensivos;
-        var faltas;
+    constructor(posicion, victorias, derrotas, puntos, asistencias, rebotesofensivos, rebotesdefensivos, faltas){
+        this.posicion=posicion;
+        this.victorias=victorias;
+        this.derrotas=derrotas;
+        this.puntos=puntos;
+        this.asistencias=asistencias;
+        this.rebotesofensivos=rebotesofensivos;
+        this.rebotesdefensivos=rebotesdefensivos;
+        this.faltas=faltas;
     }
 }
